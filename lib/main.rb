c40b98d504a3508aa2c42601f67068d348b9c9d5
«kirillshved» <kirillshved@coherentsolutions.com>
2017-04-19 14:22:06 +0300

    anded fix

--- lib/main.rb
+++ lib/main.rb
@@ -3,8 +3,8 @@ require 'flickraw'
 require 'open-uri'
 require 'rmagick'
 
-FlickRaw.api_key = "9ce4f8db4b2a189b79d79e1b88a7b504"
-FlickRaw.shared_secret = "0a3c83e80245f931"
+FlickRaw.api_key = '9ce4f8db4b2a189b79d79e1b88a7b504'
+FlickRaw.shared_secret = '0a3c83e80245f931'
 
 class Main
 
@@ -16,8 +16,10 @@ class Main
   end
 
   def run
+    print 'Please enter a #tag: '
     urls
-    puts @urls
+    downloads
+    collage
   end
 
   private
@@ -30,6 +32,43 @@ class Main
     @urls = @urls.map { |id| flickr.photos.getInfo(photo_id: id) }
 
     @urls = @urls.map { |picture| FlickRaw.url(picture) }
+  end
+
+  def downloads
+    @urls.each_with_index do |image, index|
+      open(image) do |f|
+        File.open('../img/' + index.to_s + '.jpg', 'wb') do |file|
+          file.puts f.read
+        end
+      end
+    end
+  end
+
+  def collage
+    a = Magick::ImageList.new
+
+    image = '0'
+    10.times do
+      a.read('../img/' + image + '.jpg')
+      image.succ!
+    end
+
+    collage = Magick::ImageList.new
+    page = Magick::Rectangle.new(0, 0, 0, 0)
+    a.scene = 0
+    5.times do |i|
+      2.times do |j|
+        collage << a.scale(0.25)
+        page.x = j * collage.columns
+        page.y = i * collage.rows
+        collage.page = page
+        (a.scene += 1) rescue a.scene = 0
+      end
+    end
+
+    collage.mosaic.write('../img/collage.jpg')
+
+    exit
   end
 end
 
